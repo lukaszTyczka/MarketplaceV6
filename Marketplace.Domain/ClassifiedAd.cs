a13e8b63f9cdab29b0de9507443f421070a8a5e1
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-13 21:42:44 +0100

    Add ClassifiedAd

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
