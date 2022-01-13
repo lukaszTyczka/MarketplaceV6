4e72969f3439325980d1e6bb5188f4de9a172869
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-13 22:06:44 +0100

    Use UserId in classifiedAd

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -9,13 +9,12 @@ namespace Marketplace.Domain
     public class ClassifiedAd
     {
         public Guid Id { get; }
+        private UserId _ownerId;
 
-        public ClassifiedAd(Guid id, Guid ownerId)
+        public ClassifiedAd(Guid id, UserId ownerId)
         {
             if(id == default)
                 throw new ArgumentException("Identity must be specified", nameof(id));
-            if(_ownerId == default)
-                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
             Id = id;
             _ownerId = ownerId;
         }
@@ -24,7 +23,7 @@ namespace Marketplace.Domain
         public void UpdateText(string text) => _text = text;
         public void UpdatePrice(decimal price) => _price = price;
 
-        private Guid _ownerId;
+        
         private string _title;
         private string _text;
         private decimal _price;
