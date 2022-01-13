7d0a00fc701349e9754ee8200896ea18a8bcf4eb
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-13 21:44:47 +0100

    Add constructor with Id

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -10,6 +10,13 @@ namespace Marketplace.Domain
     {
         public Guid Id { get; private set; }
 
+        public ClassifiedAd(Guid id)
+        {
+            if(id == default)
+                throw new ArgumentNullException("Identity must be specified", nameof(id));
+            Id = id;
+        }
+
         private Guid _ownerId;
         private string _title;
         private string _text;
