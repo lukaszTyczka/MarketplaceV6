ed3636a44b170ef2e33f799a7a4efd0f71cfdf6e
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-15 11:51:13 +0100

    Add Value abstract class

---
+++ Marketplace.Framework/Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Framework;
+public abstract class Value<T> where T : Value<T>
+{
+}
