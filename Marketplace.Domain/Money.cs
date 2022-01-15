1f5faccda12c545809afed58b80f5119fc4b15cf
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-15 11:20:18 +0100

    Add Money class

---
+++ Marketplace.Domain/Money.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain;
+
+public class Money
+{
+    public decimal Amount { get; }
+    public Money(decimal amount) => Amount = amount;
+}
