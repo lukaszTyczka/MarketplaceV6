1ee24c9944b2112fc41bf51f15cc6920c54e5682
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-15 11:27:20 +0100

    Add money class equals test red

--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using FluentAssertions;
+using Marketplace.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Marketplace.Tests;
 
 public class MoneyTest
 {
-
+    [Fact]
+    public void MoneyObjectsWithTheSameAmountShouldBeEqual()
+    {
+        var firstAmount = new Money(5);
+        var secondAmount = new Money(5);
+        firstAmount.Should().Be(secondAmount, "Because Object has te same amount");
+    }
 }
 
