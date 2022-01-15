f8bc6c9c5def5bb3504243cb046bb579c7944517
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-15 11:35:24 +0100

    MoneyTest equal Green

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Marketplace.Domain;
+﻿namespace Marketplace.Domain;
 
 public class Money
 {
     public decimal Amount { get; }
     public Money(decimal amount) => Amount = amount;
+
+    public bool Equals(Money other)
+    {
+        if(ReferenceEquals(null, other)) return false;
+        if(ReferenceEquals(this, other)) return true;
+        return Amount.Equals(other.Amount);
+    }
+    public override bool Equals(object? obj)
+    {
+        if(ReferenceEquals (null, obj)) return false;
+        if(ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((Money)obj);
+    }
+    public override int GetHashCode() => Amount.GetHashCode();
+    public static bool operator == (Money left, Money right) => Equals(left, right);
+    public static bool operator != (Money left, Money right) => !Equals(left, right);
 }
