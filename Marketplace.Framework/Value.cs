88d1c931c2819a1bb119212cfe6af812520bad3b
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-15 12:07:48 +0100

    Add rest of value class

--- Marketplace.Framework/Value.cs
+++ Marketplace.Framework/Value.cs
@@ -11,10 +11,84 @@ public abstract class Value<T> where T : Value<T>
 {
     private static readonly Member[] Members = GetMembers().ToArray();
 
+    public override bool Equals(object? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        var members = Members;
+
+        return other.GetType() == typeof(T) && Members.All(m =>
+        {
+            var otherValue = m.GetValue(other);
+            var thisValue = m.GetValue(this);
+            return m.IsNonStringEnumerable
+                ? GetEnumerableValues(otherValue).SequenceEqual(GetEnumerableValues(thisValue))
+                : (otherValue?.Equals(thisValue) ?? thisValue == null);
+        });
+    }
+    public override int GetHashCode() =>
+            CombineHashCodes(
+                Members.Select(m => m.IsNonStringEnumerable
+                    ? CombineHashCodes(GetEnumerableValues(m.GetValue(this)))
+                    : m.GetValue(this)));
+
+    public static bool operator ==(Value<T> left, Value<T> right) => Equals(left, right);
+
+    public static bool operator !=(Value<T> left, Value<T> right) => !Equals(left, right);
+
+    public override string ToString()
+    {
+        if (Members.Length == 1)
+        {
+            var m = Members[0];
+            var value = m.GetValue(this);
+            return m.IsNonStringEnumerable
+                ? $"{string.Join("|", GetEnumerableValues(value))}"
+                : value.ToString();
+        }
+
+        var values = Members.Select(m =>
+        {
+            var value = m.GetValue(this);
+            return m.IsNonStringEnumerable
+                ? $"{m.Name}:{string.Join("|", GetEnumerableValues(value))}"
+                : m.Type != typeof(string)
+                    ? $"{m.Name}:{value}"
+                    : value == null
+                        ? $"{m.Name}:null"
+                        : $"{m.Name}:\"{value}\"";
+        });
+        return $"{typeof(T).Name}[{string.Join("|", values)}]";
+    }
+
     private static IEnumerable<Member> GetMembers()
     {
-        throw new NotImplementedException();
+        var t = typeof(T);
+        const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public;
+        while (t != typeof(object))
+        {
+            if (t == null) continue;
+            foreach (var p in t.GetProperties(flags)) yield return new Member(p);
+            foreach (var f in t.GetFields(flags)) yield return new Member(f);
+            t = t.BaseType;
+        }
     }
+
+    private static IEnumerable<object> GetEnumerableValues(object obj)
+    {
+        var enumerator = ((IEnumerable)obj).GetEnumerator();
+        while (enumerator.MoveNext()) yield return enumerator.Current;
+    }
+
+    private static int CombineHashCodes(IEnumerable<object> objs)
+    {
+        unchecked
+        {
+            return objs.Aggregate(17, (current, obj) => current * 59 + (obj?.GetHashCode() ?? 0));
+        }
+    }
+
 
     private struct Member
     {
