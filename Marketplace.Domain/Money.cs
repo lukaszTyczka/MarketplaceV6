fded1d8671aafd0ff9bb2de128f2f9d27324663b
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-17 20:49:22 +0100

    Add operation in class money

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -6,5 +6,9 @@ public class Money : Value<Money>
 {
     public decimal Amount { get; }
     public Money(decimal amount) => Amount = amount;
+    public Money Add(Money summand) => new Money(Amount + summand.Amount);
+    public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+    public static Money operator + (Money sumand1, Money sumand2) => sumand1.Add(sumand2);
+    public static Money operator - (Money sumand1, Money sumand2) => sumand1.Subtract(sumand2);
 
 }
