2c61ba37bd64e817f152a9198511301323428657
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-17 20:51:46 +0100

    Add test to add Money

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -9,6 +9,7 @@ public class Money : Value<Money>
     public Money Add(Money summand) => new Money(Amount + summand.Amount);
     public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
     public static Money operator + (Money sumand1, Money sumand2) => sumand1.Add(sumand2);
-    public static Money operator - (Money sumand1, Money sumand2) => sumand1.Subtract(sumand2);
+    public static Money operator - (Money sumand1, Money sumand2) => sumand1.Subtract
+        (sumand2);
 
 }
--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -18,5 +18,16 @@ public class MoneyTest
         var secondAmount = new Money(5);
         firstAmount.Should().Be(secondAmount, "Because Object has te same amount");
     }
+
+    [Fact]
+    public void SumOfMoneyGiveFullAmount()
+    {
+        var coin1 = new Money(1);
+        var coin2 = new Money(2);
+        var coin3 = new Money(2);
+        var banknot = new Money(5);
+
+        banknot.Should().Be(coin1+ coin2 + coin3);
+    }
 }
 
