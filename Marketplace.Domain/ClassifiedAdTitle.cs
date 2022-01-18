19ac4efc7ca2a2e4c5386b0eace0eafc728f265d
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-18 19:40:38 +0100

    Add classifiedAdTitle class

---
+++ Marketplace.Domain/ClassifiedAdTitle.cs
@@ -0,0 +1,21 @@
+﻿using Marketplace.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAdTitle : Value<ClassifiedAdTitle>
+    {
+        public static ClassifiedAdTitle FromString(string title) => new ClassifiedAdTitle(title);
+        private readonly string _value;
+        private ClassifiedAdTitle(string value)
+        {
+            if(value.Length > 100)
+                throw new ArgumentOutOfRangeException("Title cannot be longer that 100 characters", nameof(value));
+            _value = value;
+        }
+    }
+}
