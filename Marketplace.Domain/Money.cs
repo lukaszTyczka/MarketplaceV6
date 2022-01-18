31da09e1328e41bcd5da761da0e194f647d20716
Łukasz Tyczka <lukasz@tyczka.net>
2022-01-18 19:47:59 +0100

    Add factory metod to Money and check decimals

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -4,8 +4,17 @@ namespace Marketplace.Domain;
 
 public class Money : Value<Money>
 {
+    public static Money FromDecimal(decimal amount) => new Money(amount);
+    public static Money FromString(string amount) => new Money(decimal.Parse(amount));
+
+    protected Money(decimal amount) 
+    { 
+        if(decimal.Round(amount, 2) != amount)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount cannot have more than two decimals");
+        Amount = amount; 
+    }
+
     public decimal Amount { get; }
-    public Money(decimal amount) => Amount = amount;
     public Money Add(Money summand) => new Money(Amount + summand.Amount);
     public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
     public static Money operator + (Money sumand1, Money sumand2) => sumand1.Add(sumand2);
--- Marketplace.Tests/MoneyTest.cs
+++ Marketplace.Tests/MoneyTest.cs
@@ -14,18 +14,18 @@ public class MoneyTest
     [Fact]
     public void MoneyObjectsWithTheSameAmountShouldBeEqual()
     {
-        var firstAmount = new Money(5);
-        var secondAmount = new Money(5);
+        var firstAmount = Money.FromDecimal(5);
+        var secondAmount = Money.FromDecimal(5);
         firstAmount.Should().Be(secondAmount, "Because Object has te same amount");
     }
 
     [Fact]
     public void SumOfMoneyGiveFullAmount()
     {
-        var coin1 = new Money(1);
-        var coin2 = new Money(2);
-        var coin3 = new Money(2);
-        var banknot = new Money(5);
+        var coin1 = Money.FromDecimal(1);
+        var coin2 = Money.FromDecimal(2);
+        var coin3 = Money.FromDecimal(2);
+        var banknot = Money.FromDecimal(5);
 
         banknot.Should().Be(coin1+ coin2 + coin3);
     }
@@ -33,9 +33,9 @@ public class MoneyTest
     [Fact]
     public void SubtractOfMoneyGiveAmount()
     {
-        var banknot = new Money(10);
-        var banknot2 = new Money(5);
-        var resultBanknot = new Money(5);
+        var banknot = Money.FromDecimal(10);
+        var banknot2 = Money.FromDecimal(5);
+        var resultBanknot = Money.FromDecimal(5);
 
         resultBanknot.Should().Be(banknot - banknot2);
     }
